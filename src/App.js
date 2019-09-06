0f3e6fc7fd2b6835c4abce26a8c2ffa889e5e227
VictorFaste <jplcsgo@hotmail.com>
2019-09-06 18:29:33 -0300

    ReactJs Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
+import logo3 from "./logo3.png";
+import Header from "./componentes/Header/index";
+import NavBar from "./componentes/Bar/index.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-logo"></div>
+        <NavBar />
       </header>
+
+      <div className="Icon">
+        <img src={logo3} alt="fireSpot" />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Header/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <header id="main-header">JsHunter </header>;
+
+export default Header;
